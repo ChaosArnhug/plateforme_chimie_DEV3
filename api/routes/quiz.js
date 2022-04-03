3c4e43f1bb9e4c052054f2d303ccf0020a3f3aeb
ChaosArnhug <HE201932@students.ephec.be>
2022-04-03 15:39:50 +0200

    [ADD]Implémentation partie endpoints quiz

--- api/routes/quiz.js
+++ api/routes/quiz.js
@@ -17,21 +17,37 @@ router.get("/:cours", (req, res) =>{
         }
     })
 })
-
+//Il manque les images pour les questions et les réponses
 router.get("/:cours/:quiz_id", (req, res) =>{
-    res.send({ title: 'QUIZ_ID' });
+    database.query(`
+        select quiz.titre, quiz.description, quiz.estVisible, concat('[', group_concat('{', '"titreQuestion":"', QQ.titre, '",', '"enonce":"', QQ.enonce, '",', '"estQCM":"', QQ.estQCM, '",', '"points":', QQ.points, ',', '"reponses":',
+	        (select concat('[',group_concat('{','"texteReponse":"',reponses.texteResponse,'",', '"estCorrecte":',reponses.estCorrecte, '}'), ']' ) from questions QR
+	        inner join reponses on reponses.idQuestions = QR.idQuestions
+	        where QR.idQuestions = QQ.idQuestions
+	        group by QR.enonce),
+        "}"   ), ']') as questions from quiz 
+        inner join questions as QQ on QQ.idQuiz = quiz.idQuiz
+        inner join cours on cours.idCours = quiz.idCours
+        where cours.nom = '${req.params.cours}' and quiz.id = '${req.params.quiz_id}'
+        group by quiz.idQuiz `, (err, rows, fields) => {
+
+        if (! err){
+            res.send(rows);
+        }else{
+            res.send("An error occured");
+            console.log(err);
+        }
+    })
 })
 
+//Faut qu'on discute du fonctionnement des quizs
 router.post("/{quiz_id}", (req, res) =>{
     res.send({ title: 'QUIZ_ID' });
 })
 
-router.get("/creation", (req, res) =>{
-    res.send({ title: 'QUIZ_CREATION' });
-})
-
+//Same que précédement 
 router.post("/creation", (req, res) =>{
     res.send({ title: 'QUIZ_CREATION' });
 })
 
-module.exports = router
+module.exports = router;
