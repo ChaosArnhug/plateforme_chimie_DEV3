24dea7dda16b8d45a92ef968e8b00aaa91feb38c
Ikram <HE201916@students.ephec.be>
2022-05-01 15:20:43 +0200

    modified:   client/src/components/QuizEleve/QuizMain.css 	modified:   client/src/components/QuizEleve/QuizMain.js 	modified:   client/src/components/QuizEleve/question/question.css 	modified:   client/src/components/QuizEleve/question/question.js 	modified:   client/src/index.css 	client/src/components/QuizEleve/question/Result.js

--- client/src/components/QuizEleve/question/question.js
+++ client/src/components/QuizEleve/question/question.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import './Question.css';
-import Answer from '../answer/Answer'
+import React, {useState} from "react";
+import './question.css';
 
-const Question = (props) => {
-    
-    let question = Object.keys(props.questions)
-    .map((qQuestion, i) =>(
-        <>
-             <h1>{props.questions[qQuestion].titreQuestion}: {props.questions[qQuestion].enonce}</h1>
-            <Answer
-                answers = {props.questions[qQuestion].reponses}
-                estQCM = {props.questions[qQuestion].estQCM}
-            />
-        </>
-    ))
-    
-    return question;
-   
-}
+const Question = ({titreQuestion, isQCM, enonce, points, reponses}) => {
+  //const [answer,isQCM] = useState(question.reponses, question.estQCM);
+
+  //Parcours la liste des reponses et les affiche sous forme de liste ou un onglet input
+    let answers= reponses.map((answer, index) => (    
+        isQCM === "1" ?  // si la question est un QCM on affiche la liste des choix
+            <>
+                <li
+                    key={answer.texteReponse}
+                    onClick={() => {
+                       // setAnswer([texteReponse]);
+                        //selected(texteReponse);
+                    }}
+                >
+                    {answer.texteReponse}
+                </li>
+                </>
+             //sinon on affiche input   
+            :<input type="text" id="fname" name="fname" >
+            </input>
+        ));
+        
+ 
+
+  return (
+      // renvoi le HTML de la liste des questions et les choix possible/ onglet input
+    <div className="questionBox">
+       
+      <h1 className="Answers">{titreQuestion}: {enonce}  ? </h1>
+       <ul className="Answers">
+        {answers}
+      </ul>
+    </div>
+  );
+};
 
 export default Question;
