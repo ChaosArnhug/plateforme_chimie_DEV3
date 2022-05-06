de7559736d6d783a3fd827ca4d8563e39a47ad0a
Ikram <HE201916@students.ephec.be>
2022-05-06 22:46:15 +0200

    upd client/src/components/QuizEleve/QuizMain.js

--- client/src/components/QuizEleve/QuizMain.js
+++ client/src/components/QuizEleve/QuizMain.js
@@ -1,8 +1,7 @@
 import React, {Component } from 'react';
 import './QuizMain.css';
-import Question from "./question/question";
-import Result from "./question/question.js"
-
+import Question from "./Question/Question";
+import Result from "./Question/Result"
 
 export default class Quiz extends Component {
 
@@ -10,61 +9,100 @@ export default class Quiz extends Component {
    
     state = {
             questionList: [],
-            titre: '',
-            description:'',
-            estVisible: 0,
             score:0,
-            responses:0,
-            loading:true
+            responses:[],
+            titre:"",
+            description:""
+
+           
     };
     
-    //recuperation des questions d'un quiz (appel de l'API) ex: http://localhost:5000/quiz/les%20molécules/1
+       //recuperation des questions d'un quiz (appel de l'API) ex: http://localhost:5000/quiz/les%20molécules/1
     async getQuestions (cour, quizid)  {  
-      const url = "http://localhost:5000/quiz/"+cour+"/"+quizid;
-      const response = await fetch(url);
-      const data = await response.json();
-      this.setState({loading : false, questionList : JSON.parse(data[0].questions), titre: data[0].titre, description:data[0].description, estVisible:data[0].estVisible });
+        const url = "http://localhost:5000/quiz/"+cour+"/"+quizid;
+        const response = await fetch(url);
+        console.log(response);
+        const data = await response.json();
+        this.setState({questionList : JSON.parse(data[0].questions), titre: data[0].titre, description:data[0].description });
+      };
+  
+  
+   //calcul de la note 
+   computeAnswer = (answer, correctAnswer) => {         
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
     };
 
-    //calcul de la note 
-   computeAnswer = (answer, correctAnswer) => {         
-       if (answer === correctAnswer) {
-           this.setState({
-               score: this.state.score + 1
-           });
-       }
-       this.setState({
-           responses: this.state.responses <5 ? this.state.responses +1:5
-       });
-   };
+    setAnswer = (questionid,index) => {     
+        let responses = this.state.responses;
+        //chercher si déjà selectionné
+        var indexRecord = responses.indexOf("{questionid:"+questionid+",response:"+index+"}");
+        console.log(indexRecord);
+        if (indexRecord === -1)
+            responses.push("{questionid:"+questionid+",response:"+index+"}");    
+        else
+           responses.splice(indexRecord, 1);
 
-   //refaire le quiz
-   playAgain = () => {
-       this.getQuestions();
-       this.setState({
-           score:0,
-           responses:0
-       })
-   }
+          
+       
+        this.setState({
+            responses:responses
+        });
+        console.log(this.state.responses);
+    };
+    
+    updateInputValue = (evt,questionid) => {     
+        let responses = this.state.responses;
+        let indexRecord= responses.findIndex(element => element.includes("{questionid:"+questionid+","))
+        
+        console.log(indexRecord);
+        if (indexRecord === -1)
+            responses.push("{questionid:"+questionid+",response:"+evt+"}");    
+        else {
+          responses.splice(indexRecord, 1);
+          responses.push("{questionid:"+questionid+",response:"+evt+"}"); 
+        }
 
-   //récupération des questions au chargement du composant 
-   async componentDidMount ()  {
+          
+       
+        this.setState({
+            responses:responses
+        });
+        console.log(this.state.responses);
+    };
+
+
+    //refaire le quiz
+    tryAgain = () => {
+        this.setState({
+            questionList: [],
+            score:0,
+            responses:[]
+
+        });
+        this.getQuestions("les%20molécules",1);
+          
+    }
+   
+    //récupération des questions au chargement du composant 
+    async componentDidMount ()  {
       
-      //console.log("test"+this.state.questionList)
-       this.getQuestions("les%20molécules",1);
-   }
+        //console.log("test"+this.state.questionList)
+         this.getQuestions("les%20molécules",1);
+     }
   
- 
-
-
-   render(){
-
-      let { questionList, titre, description }= this.state
+     
+    render(){
+        
+        let { questionList, titre, description }= this.state
       //const qst=JSON.parse(questions)
         return(
         
             <div className="container">
-                <div className="title">{titre}: {description}</div>
+                <div className="title" key={titre}>{titre}: {description}</div>
                 
                 {
                 
@@ -78,6 +116,9 @@ export default class Quiz extends Component {
                       enonce={question.enonce}
                       points={question.points}
                       reponses={question.reponses}
+                      questionid={i}
+                      setAnswer={this.setAnswer}
+                      updateInputValue={this.updateInputValue}
                       
                   />
                   )
@@ -90,8 +131,8 @@ export default class Quiz extends Component {
                         className="Terminer" 
                         onClick={() => {
                         ( <Result 
-                                score={this.state.score} 
-                                playAgain={this.playAgain} 
+                                responses={this.state.responses} 
+                                tryAgain={this.tryAgain} 
                             />)
                         }}
                     >
@@ -99,9 +140,13 @@ export default class Quiz extends Component {
                     
                     </button>
                 
-                {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />):null}
-                
-                
+                    <button 
+                        className="Reset" 
+                        onClick={this.tryAgain}
+                    >
+                        Reset  <i className="fa fa-undo" />
+                    
+                    </button>
             </div>
             
 
